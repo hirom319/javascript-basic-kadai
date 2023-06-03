a4d13854b0c08ae9f42929e221ce147839a79fdb
hirom319 <hmiu319@gmail.com>
2023-06-04 02:47:19 +0900

    function created

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,2 +1,12 @@
 'use strict';
 
+let weight;
+let height;
+
+function bmiCalc(weight, height) {
+    const bmi = weight / (height * height)
+    console.log(bmi)
+    return bmi
+}
+
+bmiCalc(70, 1.7)
