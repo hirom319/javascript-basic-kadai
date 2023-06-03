ea78c7f44754c3ff0a719f8f0e6728751e225f40
hirom319 <hmiu319@gmail.com>
2023-06-04 08:44:53 +0900

    js modified

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,18 @@
-'use strict';
+"use strict";
 
 let num;
 num = 15;
 
-if (num % 3 === 0 && num % 5 === 0) {
-    console.log("３と５の倍数です") 
-} else if (num % 3 === 0){
-    console.log("３の倍数です")
-} else if (num % 5 === 0) {
-    console.log("５の倍数です")
-} else {
+if (Number.isInteger(num) && num > 0) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("３と５の倍数です");
+  } else if (num % 3 === 0) {
+    console.log("３の倍数です");
+  } else if (num % 5 === 0) {
+    console.log("５の倍数です");
+  } else {
     console.log(num);
+  }
+} else {
+  console.log("正の整数ではありません");
 }
