44056b668b88db4282ae458e39ac01cfd96b4204
hirom319 <hmiu319@gmail.com>
2023-06-04 09:40:44 +0900

    js function added

--- kadai_20/event.js
+++ kadai_20/event.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const text = document.getElementById("text");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    setTimeout(function(){
+        text.textContent = "ボタンをクリックしました"
+    }, 2000);
+});
