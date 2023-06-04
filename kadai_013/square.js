c4e01707f3ffd038404037e000dc87c40e65ecea
hirom319 <hmiu319@gmail.com>
2023-06-04 14:51:39 +0900

    js file updated

--- kadai_013/square.js
+++ kadai_013/square.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// const num = 10;
-
-// function multiplierCalc(num) {
-//     const multiNum = num ** 2;
-//     return multiNum;
-// }
-
-// let result = multiplierCalc(num);
-// console.log(result);
-
-
 const multiplierCalc = function(num)  {
     console.log(num ** 2)
 }
